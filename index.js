bb4090ba3c9f1ae56a66d079e6ecf5c90bb8da5b
Jay Kumar <jaykumar3dear@gmail.com>
2025-05-13 10:11:45 +0000

    fix bugs

--- index.js
+++ index.js
@@ -1,43 +1,44 @@
-const express = require('express');
-const search = require('./searchOnYoutube');  
-const cors = require('cors');
-const dotEnv  =  require('dotenv')
-const JWT = require("jsonwebtoken")
+const express = require("express");
+const search = require("./searchOnYoutube");
+const cors = require("cors");
+const dotEnv = require("dotenv");
+const JWT = require("jsonwebtoken");
 
-dotEnv.config()
+dotEnv.config();
 
 const server = express();
 
-
-server.use(cors(
-    {
-        origin:["https://study-five-ruby.vercel.app/","http://localhost:3000","http://localhost:5173"]
-    }
-));
-
+server.use(
+  cors({
+    origin: [
+      "https://study-five-ruby.vercel.app/",
+      "http://localhost:3000",
+      "http://localhost:5173",
+    ],
+  })
+);
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
-server.get('/',(req,res)=>res.json({message:"Ok server is running"}))
+server.get("/", (req, res) => res.json({ message: "Ok server is running" }));
 server.get("/api/search", async (req, res) => {
-    const query = req.query.query;
+  const query = req.query.query;
 
-    const originalQuery = JWT.verify(query,process.env.SECRET_KEY)
-    if (!query) {
-        return res.status(400).json({ message: 'Query parameter is required' });
-    }
+  if (!query) {
+    return res.status(400).json({ message: "Query parameter is required" });
+  }
 
-    try {
-        const response = await search(originalQuery);
-        return res.status(200).json(response);
-    } catch (error) {
-        console.error('Error in search operation:', error);
-        return res.status(500).json({ message: 'Internal server error' });
-    }
+  try {
+    const response = await search(query);
+    return res.status(200).json(response);
+  } catch (error) {
+    console.error("Error in search operation:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 // Configure the port
-const PORT = process.env.PORT || 4000;  
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
